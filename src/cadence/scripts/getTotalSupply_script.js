d3c7d1a7920dcb77122c9468a4e46aa139b0bd71
dev <69097418+markobonna@users.noreply.github.com>
2022-07-06 11:47:37 -0700

    app

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,11 +1,11 @@
 export const getTotalSupply =
-// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-`
-import BottomShot from 0x7b6adb682517f137;
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import BottomShot from 0x7b1659f4be47a598;
 
 pub fun main(): UInt64 {
 
     return BottomShot.totalSupply;
 
 }
-`
+`;
